858b2076e275216c93705ade498b70971950b45d
Valmos22 <hugovalderrama5@gmail.com>
2024-09-15 19:48:17 -0500

    Cambio para desplegar en render

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		port: parseInt(process.env.VITE_PORT || '5174'), // Si no está definida, usa el puerto 3000
+	},
+	preview: {
+		host: '0.0.0.0',
+		port: parseInt(process.env.VITE_PORT || '5174'), // Usa el mismo puerto para el modo de preview
+	}
 });
